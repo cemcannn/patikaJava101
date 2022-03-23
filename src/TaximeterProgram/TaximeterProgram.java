0e321bc05e2f74815de12eeafd4c28707ca83f7d
cemcannn <cmcan@windowslive.com>
2022-03-23 18:07:29 +0300

    Main 3 | Program that calculate taximeter price

---
+++ src/TaximeterProgram/TaximeterProgram.java
@@ -0,0 +1,16 @@
+package TaximeterProgram;
+
+import java.util.Scanner;
+
+public class TaximeterProgram {
+    public static void main(String[] args) {
+        int distance;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter traveled distance : ");
+        distance = input.nextInt();
+        double price = 10 + (distance * 2.2);
+        boolean condition = price < 20;
+        double netPrice = condition ? 20 : price;
+        System.out.println(netPrice);
+    }
+}
