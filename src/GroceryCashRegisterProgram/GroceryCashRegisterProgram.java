082b54aaebc645ed887d9cd36d7f40f597878481
cemcannn <cmcan@windowslive.com>
2022-03-23 20:54:29 +0300

    Main 6 | Grocery Cash Register Program

--- src/GroceryCashRegisterProgram/GroceryCashRegisterProgram.java
+++ src/GroceryCashRegisterProgram/GroceryCashRegisterProgram.java
@@ -4,22 +4,18 @@ import java.util.Scanner;
 
 public class GroceryCashRegisterProgram {
     public static void main(String[] args) {
-        float pearPrice = 2.14f;
-        float applePrice = 3.67f;
-        float tomatoPrice = 1.11f;
-        float bananaPrice = 0.95f;
-        float eggplantPrice = 5.99f;
+        float pearPrice = 2.14f, applePrice = 3.67f, tomatoPrice = 1.11f, bananaPrice = 0.95f, eggplantPrice = 5.99f;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("How much kilos of pears do you want : ");
+        System.out.print("How much kilos of pears do you want : ");
         int pearAmount = input.nextInt();
-        System.out.println("How much kilos of apples do you want : ");
+        System.out.print("How much kilos of apples do you want : ");
         int appleAmount = input.nextInt();
-        System.out.println("How much kilos of tomatoes do you want : ");
+        System.out.print("How much kilos of tomatoes do you want : ");
         int tomatoAmount = input.nextInt();
-        System.out.println("How much kilos of bananas do you want : ");
+        System.out.print("How much kilos of bananas do you want : ");
         int bananaAmount = input.nextInt();
-        System.out.println("How much kilos of eggplants do you want : ");
+        System.out.print("How much kilos of eggplants do you want : ");
         int eggplantAmount = input.nextInt();
         double totalNetPrice = (pearPrice*pearAmount)+(applePrice*appleAmount)+(tomatoPrice*tomatoAmount)+(bananaPrice*bananaAmount)+(eggplantPrice*eggplantAmount);
         System.out.println(totalNetPrice);
