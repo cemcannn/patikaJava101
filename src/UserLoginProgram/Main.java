6c2978f0c17d7ebd415843155eb73acd60e92186
cemcannn <cmcan@windowslive.com>
2022-03-24 12:01:39 +0300

    Main 9 | User Login Program That Control Your Username and Password

--- src/UserLoginProgram/Main.java
+++ src/UserLoginProgram/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String username, password;
+        String username, password, select, passwordSystemNew;
 
         String usernameSystem = "user@hotmail.com";
         String passwordSystem = "123456789";
@@ -17,10 +17,24 @@ public class Main {
         password = input.nextLine();
 
         if ((username.equals(usernameSystem)) && (password.equals(passwordSystem))) {
-            System.out.println("Login Successful");
-        }
-        else {
-            System.out.println("Login Failed");
+            System.out.println("Login successful");
+        } else if (username.equals(usernameSystem)) {
+            System.out.println("Login failed." +
+                    "\n If you want to reset your password please enter '1' or skip, please enter any key.");
+            select = input.nextLine();
+            if (select.equals("1")) {
+                System.out.print("Please enter your new password that will be different old password :");
+                passwordSystemNew = input.nextLine();
+                if (passwordSystemNew.equals(passwordSystem)) {
+                    System.out.println("Password hasn't been created!");
+                } else {
+                    System.out.println("Password has been succsefully created.");
+                }
+            } else {
+                System.out.println("Logged out!");
+            }
+        } else {
+            System.out.println("Login failed, your username and password are invalid!");
         }
     }
 }
