e5b0907652a50bf217bec75b534715d071d571f8
cemcannn <cmcan@windowslive.com>
2022-04-02 14:51:34 +0300

    Main 27 | Reverse Triangle

---
+++ src/ReverseTriangle/Main.java
@@ -0,0 +1,22 @@
+package ReverseTriangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please input 'n' value : ");
+        n = input.nextInt();
+
+        for(int i = n; i>=1; i--) {
+            System.out.println();
+            for(int j = 1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = (2*i)-1; k>=1; k--) {
+                System.out.print("*");
+            }
+        }
+    }
+}
