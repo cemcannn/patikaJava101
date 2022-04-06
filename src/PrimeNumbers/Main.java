c6087ad19d7a56612c0bfa500aaad894a9dcbd5e
cemcannn <cmcan@windowslive.com>
2022-04-06 09:43:04 +0300

    Main 30 | Program that find prime numbers between 1 and 100

--- src/PrimeNumbers/Main.java
+++ src/PrimeNumbers/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i = 2; i<=100; i++) {
+            int count = 0;
             for(int j = 2; j<=i-1; j++) {
                 if(i%j==0) {
-                    break;
+                    count++;
                 }
+            }
+            if(count==0){
+                System.out.print(i + " ");
             }
         }
     }
