caf69ee63615d1f7f26f6ce766e9074a46991d21
cemcannn <cmcan@windowslive.com>
2022-04-17 10:25:46 +0300

    Main 40 | Program that print letter "B" with multidimensional arrays.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] scores = new int[10];
-        scores = new int[3];
-        scores = new int[]{215, 234, 218, 189, 221, 290};
-
-        for (int score : scores) {
-            System.out.print(score + "  ");
+        Scanner input = new Scanner(System.in);
+        int[] arr = {15, 12, 788, 1, -1, -778, 2, 0};
+        Arrays.sort(arr);
+        int sum = 0;
+        int sum1 = 0;
+        System.out.print("Girilen sayı : ");
+        int num = input.nextInt();
+        for (int i : arr) {
+            if (num > i) {
+                sum = i;
+            }
+            if (num < i) {
+                sum1 = i;
+                break;
+            }
         }
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + sum);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + sum1);
     }
 }
---
+++ src/ProgramThatPrintLetterBWithMultidimensionalArrays/README.md
@@ -0,0 +1,11 @@
+### Ödev
+Çok boyutlu diziler kullanılarak "yıldızlar" ile ekrana "B" harfi yazan programı yazınız.
+```
+ *  *  *  *
+ *        *
+ *        *
+ *  *  *  *
+ *        *
+ *        *
+ *  *  *  *
+ ```
